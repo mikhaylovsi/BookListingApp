b553ed2a26e9e84402f67849d1c3d0ee18ce918c
Михайлов Сергей <marrog1@mail.ru>
2017-07-27 10:27:50 +0300

    Added item xml and binding ability

--- app/src/main/java/com/example/sergei/booklistingapp/MainActivity.java
+++ app/src/main/java/com/example/sergei/booklistingapp/MainActivity.java
@@ -1,13 +1,23 @@
 package com.example.sergei.booklistingapp;
 
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.sergei.booklistingapp.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+
+
+
+
     }
 }
